c4193bec8462f8f6c12a5131c255786279a21698
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2025-01-17 09:44:31 +0530

    testing

--- src/pages/Homepage/HeroSection.jsx
+++ src/pages/Homepage/HeroSection.jsx
@@ -5,19 +5,19 @@ import TestimonialSlider from "./Testinomials";
 import Speciality from "./Speciality";
 import AccommodationList from "./Accomodation";
 
-const SnowflakeAnimation = React.lazy(() => import("./SnowFlakeAnimation"));
+// const SnowflakeAnimation = React.lazy(() => import("./SnowFlakeAnimation"));
 
 export default function HeroSection() {
   return (
     <div className="">
-        <SnowflakeAnimation />
-      <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2  w-[75%]">
+        {/* <SnowflakeAnimation />
         <BookingMenu />
-      </div>
         <CountElement />
-        <Speciality />
+        <Speciality /> */}
+      <div className="mt-8">
         <AccommodationList />
-        <TestimonialSlider />
+      </div>
+        {/* <TestimonialSlider /> */}
     </div>
   );
 }
